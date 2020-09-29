741ea3459761e29267440b17d354616865efab67
Francesco Porcari <francesco.porcari@softwell.it>
2020-09-29 14:24:31 +0200

    f

--- instances/hsh/root.py
+++ instances/hsh/root.py
@@ -4,6 +4,7 @@ sys.stdout = sys.stderr
 from gnr.web.gnrwsgisite import GnrWsgiSite
 site = GnrWsgiSite(__file__)
 
+
 def application(environ,start_response):
     return site(environ,start_response)
 
